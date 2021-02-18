5734518c831936208b904471a2239755cc7a7e02
sergei <tustanovskiis@gmail.com>
2021-02-18 10:59:53 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -4,46 +4,9 @@
       <v-app-bar color="indigo" dark>
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Nails AUSTRALIA</v-toolbar-title>
+        <v-toolbar-title><a class="white--text" :href="baseUrl">Nails AUSTRALIA</a></v-toolbar-title>
       </v-app-bar>
-
-      <v-navigation-drawer v-model="drawer" absolute temporary>
-        <v-list nav dense>
-          <v-list-item-group v-model="group" active-class="red--text text--accent-4">
-            <v-list-item @click="goTo('Home')">
-              <v-list-item-icon>
-                <v-icon>mdi-home</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item>
-
-            <v-list-item @click="goTo('offline-courses')">
-              <v-list-item-icon>
-                <v-icon>$offlineCourse</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Offline courses</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="goTo('online-courses')">
-              <v-list-item-icon>
-                <v-icon>$onlineCourse</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Online courses</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="goTo('shop')">
-              <v-list-item-icon>
-                <v-icon>$shop</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Shop</v-list-item-title>
-            </v-list-item>
-            <v-list-item @click="goTo('shop-categories')">
-              <v-list-item-icon>
-                <v-icon>$shopCategories</v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>Shop categories</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
+      <NavigationDrawer :drawer.sync="drawer"/>
       <v-main class="homefone main-content">
         <v-container>
           <component :is="layout">
@@ -62,23 +25,19 @@
 <script>
 import { mapState } from 'vuex';
 
-import CoursesLayout from './Layouts/CoursesLayout.vue';
-import ShopLayout from './Layouts/ShopLayout.vue';
-import DefaultLayout from './Layouts/DefaultLayout.vue';
-
 export default {
   name: 'App',
   data() {
     return {
       drawer: false,
-      group: null,
     };
   },
   components: {
-    CoursesLayout,
-    ShopLayout,
-    DefaultLayout,
+    CoursesLayout: () => import('@/Layouts/CoursesLayout.vue'),
+    ShopLayout: () => import('@/Layouts/ShopLayout.vue'),
+    DefaultLayout: () => import('@/Layouts/DefaultLayout.vue'),
     UploadFiles: () => import('@/components/popups/UploadFiles.vue'),
+    NavigationDrawer: () => import('@/components/NavigationDrawer.vue'),
   },
   computed: {
     layout() {
@@ -86,16 +45,13 @@ export default {
     },
     ...mapState('onlineCourses', ['uploadDialog']),
     ...mapState('user', ['user']),
+    baseUrl() {
+      return process.env.BASE_URL
+      // return process.env.VUE_APP_API_URL
+    }
   },
-  watch: {
-    group() {
-      this.drawer = false;
-    },
-  },
+
   methods: {
-    goTo(name) {
-      if (this.$route.name !== name) this.$router.push({ name });
-    },
     redirect() {
       let redirectPath = localStorage.getItem('redirectPath');
       localStorage.removeItem('redirectPath');
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-navigation-drawer v-model="localDrawer" absolute temporary>
+    <v-list nav dense>
+      <v-list-item-group v-model="group" active-class="red--text text--accent-4">
+        <v-list-item @click="goTo('Home')">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item @click="goTo('offline-courses')">
+          <v-list-item-icon>
+            <v-icon>$offlineCourse</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Offline courses</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="goTo('online-courses')">
+          <v-list-item-icon>
+            <v-icon>$onlineCourse</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Online courses</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="goTo('shop')">
+          <v-list-item-icon>
+            <v-icon>$shop</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Shop</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="goTo('shop-categories')">
+          <v-list-item-icon>
+            <v-icon>$shopCategories</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Shop categories</v-list-item-title>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    drawer:{
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return{
+      group: null,
+    }
+  },
+  computed:{
+    localDrawer: {
+      get(){
+        return this.drawer
+      },
+      set(val){
+        this.$emit('update:drawer', val)
+      }
+    }
+  },
+   watch: {
+    group() {
+      this.localDrawer = false;
+    },
+  },
+  methods:{
+    goTo(name) {
+      if (this.$route.name !== name) this.$router.push({ name });
+    },
+  }
+
+}
+</script>
+
+<style>
+</style>
