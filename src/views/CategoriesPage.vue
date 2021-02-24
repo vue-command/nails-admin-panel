f9b477bff62ac80ed55745db101e65f71172aece
sergei <tustanovskiis@gmail.com>
2021-02-24 15:23:09 +0200

    update subcategory

--- src/views/CategoriesPage.vue
+++ src/views/CategoriesPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container style="max-width: 960px;">
+  <v-container style="max-width: 960px">
     <v-row class="pa-5">
       <v-col cols="12" class="pa-5">
         <v-row>
@@ -7,7 +7,7 @@
             <v-select
               class="text-subtitle-1"
               outlined
-              :value="selectedCategory"
+              :value="selectedCategoryId"
               :items="categories"
               item-text="name"
               item-value="_id"
@@ -18,7 +18,7 @@
             </v-select>
           </v-col>
           <v-col cols="12" md="6" class="text-right">
-            <v-btn @click="open=true" class="mt-4">
+            <v-btn @click="open = true" class="mt-4">
               ADD NEW SUBCATEGORY
               <v-icon>$plus</v-icon>
             </v-btn>
@@ -31,30 +31,51 @@
 
         <v-row>
           <v-col cols="12">
-            <v-card v-if="!subcategoriesSelect.length" class="py-4 px-6">
+            <v-card v-if="!data.length" class="py-4 px-6">
               <p class="text-subtitle-1 text-center mb-0">No subcategories here yet</p>
             </v-card>
             <v-card v-else class="pt-4 pb-6 px-6">
               <v-simple-table>
                 <tbody>
-                  <tr v-for="(subcategory) in subcategoriesSelect" :key="subcategory._id">
+                  <tr v-for="subcategory in data" :key="subcategory._id">
                     <td class="d-flex justify-between px-0">
                       <v-text-field
                         class="text-subtitle-1"
-                        :value="subcategory.name"
-                        @input="newSubcatName"
+                        v-model="subcategory.name"
                         :disabled="subcategory._id !== onchangeId"
                       />
-                      <v-btn v-if="subcategory._id === onchangeId" icon class="mt-2" @click="changeSubcategoryName(subcategory.name)">
+                      <v-btn
+                        v-if="subcategory._id === onchangeId"
+                        icon
+                        class="mt-2"
+                        @click="changeSubcategoryName(subcategory.name)"
+                      >
                         <v-icon>$done</v-icon>
                       </v-btn>
-                      <v-btn v-else icon class="mt-2" @click="edit(subcategory._id)">
+                      <v-btn
+                        v-else
+                        icon
+                        class="mt-2"
+                        :disabled="disabledBtn(subcategory._id)"
+                        @click="edit(subcategory._id)"
+                      >
                         <v-icon>$edit</v-icon>
                       </v-btn>
-                      <v-btn v-if="subcategory._id === onchangeId" icon class="mt-2" @click="cancelChangeSubcategoryName(subcategory.name, subcategory._id)">
+                      <v-btn
+                        v-if="subcategory._id === onchangeId"
+                        icon
+                        class="mt-2"
+                        @click="cancelChangeSubcategoryName(subcategory._id)"
+                      >
                         <v-icon>$cancel</v-icon>
                       </v-btn>
-                      <v-btn v-else icon class="mt-2" @click="removeSubcat(subcategory._id)">
+                      <v-btn
+                        v-else
+                        icon
+                        class="mt-2"
+                        :disabled="disabledBtn(subcategory._id)"
+                        @click="removeSubcat(subcategory._id)"
+                      >
                         <v-icon>$delete</v-icon>
                       </v-btn>
                     </td>
@@ -76,39 +97,45 @@ export default {
   name: 'categories',
   components: {
     AddSubcategory: () => import('@/components/inputs/AddSubcategory.vue'),
-    confirmDelete: () => import('@/components/popups/confirmDelete.vue')
+    confirmDelete: () => import('@/components/popups/confirmDelete.vue'),
   },
   data() {
     return {
+      data: [],
       open: false,
       showDialog: false,
       title: 'subcategory',
       onchangeId: '',
       deleteId: '',
-      newSubcategoryName: '',
+      // newSubcategoryName: '',
     };
   },
   computed: {
-    ...mapState('categories', [
-      'isPageLoading',
-      'categories',
-      'activeCategory',
-    ]),
-    subcategoriesSelect() {
-      if (!this.activeCategory) return '';
-      return [ ...this.activeCategory.subcategories];
-    },
-    selectedCategory() {
+    ...mapState('categories', ['isPageLoading', 'categories', 'activeCategory']),
+    selectedCategoryId() {
       if (!this.activeCategory) return '';
       return this.activeCategory._id;
     },
   },
-
+  watch: {
+    activeCategory: {
+      deep: true,
+      handler(val) {
+        if (!val) return;
+        this.fillingData();
+      },
+    },
+  },
   methods: {
-    selectCategory(categoryId) {
-      this.$store.dispatch('categories/SET_CATEGORY', {
+    disabledBtn(id) {
+      if (!this.onchangeId) return false;
+      return id !== this.onchangeId;
+    },
+    async selectCategory(categoryId) {
+      await this.$store.dispatch('categories/SET_CATEGORY', {
         categoryId,
       });
+      this.fillingData();
     },
     removeSubcat(id) {
       this.deleteId = id;
@@ -117,32 +144,32 @@ export default {
     edit(id) {
       this.onchangeId = id;
     },
-    newSubcatName(value){
-      this.newSubcategoryName = value;
-    },
     async changeSubcategoryName(name) {
-      if (this.newSubcategoryName.trim() && this.newSubcategoryName !== name) {
-        this.$store.dispatch('categories/CHANGE_SUBCATOGORY_NAME', { id: this.onchangeId, name: this.newSubcategoryName });
-      } else {
-        this.newSubcategoryName = name;
-      }
+      const newName = name.trim();
+      if (!newName) return;
+      const oldName = this.activeCategory.subcategories.find(item => item._id === this.selectedCategoryId)?.name;
+      if (oldName === newName) return;
+      await this.$store.dispatch('categories/CHANGE_SUBCATOGORY_NAME', { id: this.onchangeId, name: newName });
       this.onchangeId = '';
     },
     async deleteSubcategoryHandler() {
-      this.$store.dispatch('categories/DELETE_SUBCATOGORY', { id: this.deleteId });
+      await this.$store.dispatch('categories/DELETE_SUBCATOGORY', { id: this.deleteId });
       this.showDialog = false;
     },
-    cancelChangeSubcategoryName(name, id) {
-      this.newSubcategoryName = name;
+    cancelChangeSubcategoryName(id) {
+      const obj1 = this.activeCategory.subcategories.find(elem => elem._id === id);
+      const obj2 = this.data.find(elem => elem._id === id);
+      Object.assign(obj2, obj1);
       this.onchangeId = '';
-      this.activeCategory.subcategories.find((elem) => elem._id === id).name = name
-    }
+    },
+    fillingData() {
+      if (!this.selectedCategoryId) return;
+      this.data = this.activeCategory.subcategories.map(item => Object.assign({}, item));
+    },
   },
-
-  updated() {
-    if (!this.activeCategory) return '';
-    return [ ...this.activeCategory.subcategories];
-  }
+  mounted() {
+    this.fillingData();
+  },
 };
 </script>
 
